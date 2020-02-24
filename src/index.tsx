00103dcff0a76e84e66461a4415cd61826df0ff2
tajpouria <tajpouria.dev@gmail.com>
2020-02-24 11:57:15 +0330

    Addde -- async-mobx

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,47 @@
 import * as React from "react";
 import { render } from "react-dom";
+import { observer } from "mobx-react";
 
+import { weatherStore } from "./stores/weatherStore";
 import "./styles.css";
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
-  );
+@observer
+class App extends React.Component<any> {
+  state = {
+    city: "",
+  };
+
+  render() {
+    const {
+      weatherData,
+      loadWeather,
+      asyncLoadCity,
+      generatorLoadCity,
+    } = this.props.store;
+
+    return (
+      <>
+        <input
+          onChange={e => this.setState({ city: e.target.value })}
+          placeholder="city"
+        />
+        <button
+          onClick={() => {
+            const { city } = this.state;
+            if (city && city.trim()) {
+              // loadWeather(city);
+              // asyncLoadCity(city);
+              generatorLoadCity(city);
+            }
+          }}
+        >
+          fetch
+        </button>
+        <pre>{JSON.stringify(weatherData, null, 2)}</pre>
+      </>
+    );
+  }
 }
 
 const rootElement = document.getElementById("root");
-render(<App />, rootElement);
+render(<App store={weatherStore} />, rootElement);
--- src/stores/weatherStore.ts
+++ src/stores/weatherStore.ts
@@ -1,8 +1,3 @@
-# Async MobX
-
-./weatherStore.ts
-
-```ts
 import {
   configure, // set some global mobx config settings
   action,
@@ -65,56 +60,3 @@ decorate(WeatherStore, {
 });
 
 export const weatherStore = new WeatherStore();
-```
-
-./App.tsx
-
-```ts
-import * as React from "react";
-import { render } from "react-dom";
-import { observer } from "mobx-react";
-
-import { weatherStore } from "./stores/weatherStore";
-import "./styles.css";
-
-@observer
-class App extends React.Component<any> {
-  state = {
-    city: "",
-  };
-
-  render() {
-    const {
-      weatherData,
-      loadWeather,
-      asyncLoadCity,
-      generatorLoadCity,
-    } = this.props.store;
-
-    return (
-      <>
-        <input
-          onChange={e => this.setState({ city: e.target.value })}
-          placeholder="city"
-        />
-        <button
-          onClick={() => {
-            const { city } = this.state;
-            if (city && city.trim()) {
-              // loadWeather(city);
-              // asyncLoadCity(city);
-              generatorLoadCity(city);
-            }
-          }}
-        >
-          fetch
-        </button>
-        <pre>{JSON.stringify(weatherData, null, 2)}</pre>
-      </>
-    );
-  }
-}
-
-const rootElement = document.getElementById("root");
-render(<App store={weatherStore} />, rootElement);
-```
