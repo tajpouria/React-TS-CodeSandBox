09510fdfd36034ff95ee53ba66f1a35fdf97cd22
tajpouria <tajpouria.dev@gmail.com>
2020-05-03 12:28:23 +0430

    Added dragndropfile

--- README.md
+++ README.md
@@ -1,19 +1,3 @@
-- [Error Boundary Intro](https://github.com/tajpouria/React-TS-CodeSandBox/tree/errorboundyIntro)
+https://www.npmjs.com/package/papaparse
 
-- [Uncontrolled Components](https://github.com/tajpouria/React-TS-CodeSandBox/tree/uncontrolledComponet)
-
-- [Conway's game of life](https://github.com/tajpouria/React-TS-CodeSandBox/tree/conwaysgameoflife)
-
-- [Async MobX](https://github.com/tajpouria/React-TS-CodeSandBox/tree/async-mobx)
-
-- [Stop Using is Loading boolean!](https://github.com/tajpouria/React-TS-CodeSandBox/tree/StopUsingIsLoadingBoolean)
-
-- [React Query demo](https://github.com/tajpouria/React-TS-CodeSandBox/tree/reactQuery)
-
-- [Gqless](https://github.com/tajpouria/React-TS-CodeSandBox/tree/gqless)
-
-- [TailwindGallery](https://github.com/tajpouria/React-TS-CodeSandBox/tree/tailwindGallery)
-
-- [Next Hooks](https://github.com/tajpouria/React-TS-CodeSandBox/tree/nextHooks)
-
-- [Strapi Demo](https://github.com/tajpouria/React-TS-CodeSandBox/tree/strapiDemo)
+https://react-dropzone.js.org/ I'm not using this one but seems like pretty neat
--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,45 @@
 import * as React from "react";
 import { render } from "react-dom";
+import { parse } from "papaparse";
 
 import "./styles.css";
 
 function App() {
+  const [contacts, setContancts] = React.useState<
+    { name: string; email: string }[]
+  >([]);
+  const [heighted, setHighlited] = React.useState(false);
+
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <div
+        className="drag"
+        onDragEnter={() => setHighlited(true)}
+        onDragLeave={() => setHighlited(false)}
+        onDragOver={e => e.preventDefault()}
+        onDrop={e => {
+          e.preventDefault();
+          Array.from(e.dataTransfer.files)
+            .filter((file: File) => file.type === "text/csv")
+            .forEach(async file => {
+              const text = await file.text();
+              setContancts(ex => [
+                ...ex,
+                ...parse(text, { header: true }).data // Include csv headers
+              ]);
+            });
+        }}
+        style={{ opacity: heighted ? 0.8 : 1 }}
+      >
+        Drop here
+      </div>
+      <div>
+        {contacts.map(p => (
+          <p key={Math.random()}>
+            <b>{p.name}</b>: {p.email}
+          </p>
+        ))}
+      </div>
     </div>
   );
 }
